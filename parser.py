f39c5195083ef30158dd1e46fc7f4fd611d34f69
YaTokha <tokhaokha0903@gmail.com>
2024-04-03 22:43:45 +0300

    Доработан класс parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,34 @@
-import easyocr
+import cv2
+from cv2 import cvtColor, COLOR_BGR2GRAY, imread, THRESH_BINARY, threshold, equalizeHist
+from easyocr import Reader
+
+def optimize_image_for_ocr(image_path):
+    image = imread(image_path)
+    gray_image = cvtColor(image, COLOR_BGR2GRAY)
+    equalized_image = equalizeHist(gray_image)
+    _, optimized_image = threshold(equalized_image, 128, 255, THRESH_BINARY + THRESH_BINARY)
+    optimized_image_path = "optimized_" + image_path
+    cv2.imwrite(optimized_image_path, optimized_image)
+    return optimized_image_path
+
+def extract_text_from_image(path_to_image, output_file='extracted_text.txt'):
+    optimized_image_path = optimize_image_for_ocr(path_to_image)
+    ocr_reader = Reader(['ru'], gpu=False)  # Использование GPU если доступно
+    text_blocks = ocr_reader.readtext(optimized_image_path, detail=0, paragraph=True)
+
+    try:
+        with open(output_file, mode="w", encoding="utf-8") as outfile:
+            for text_block in text_blocks:
+                outfile.write("{}\n\n".format(text_block))
+        return f"Результат сохранен в {output_file}"
+    except Exception as e:
+        return f"Ошибка при записи файла: {e}"
+
+def execute_ocr():
+    image_path = 'optimized_img.png'
+    result_message = extract_text_from_image(image_path, 'final_output.txt')
+    print(result_message)
 
 
-def textrec(filepath, text_file_name='result.txt'):
-    reader = easyocr.Reader(['ru'])
-    result = reader.readtext(filepath, detail=0, paragraph=True)
-
-    with open(text_file_name, "w", newline='', encoding="utf-8") as file:
-        for line in result:
-            file.write(f'{line}\n\n')
-
-    return f'result {text_file_name}'
-
-
-def main():
-    filepath = 'img.jpeg'
-    print(textrec(filepath=filepath))
-
-
-if __name__ == main():
-    main()
+if __name__ == '__main__':
+    execute_ocr()
